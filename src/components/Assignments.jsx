7ef91e3dab4f60d45854b5760a557b18083118d6
ZubairAlMamun <119296138+ZubairAlMamun@users.noreply.github.com>
2024-12-22 16:07:40 +0600

    delete section added

--- src/components/Assignments.jsx
+++ src/components/Assignments.jsx
@@ -1,16 +1,64 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Footer from './Footer'
 import NavBar from './Navbar'
 import { useLoaderData } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import { AuthContext } from '../provider/AuthProvider'
 
 const Assignments = () => {
-  const data=useLoaderData();
+  const loadeddata=useLoaderData();
+  const {user}=useContext(AuthContext);
+  
+  const [data,setData]=useState(loadeddata);
   console.log(data)
+  const handledelete = (_id,email) => {
+    console.log(_id,email,user.email)
+    if(email!==user.email){
+      Swal.fire({
+        title: 'Error!',
+        text: "You Can't Delete it, Cause This is not created by you!",
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
+      return;
+    }
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+    fetch(`http://localhost:5000/assignment/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.deletedCount > 0) {
+          Swal.fire({
+            title: 'Success!',
+            text: 'Assignment Deleted succesfully',
+            icon: 'success',
+            confirmButtonText: 'Cool'
+          })
+          const filtereddata = data.filter((user) => user._id !== _id);
+          setData(filtereddata);
+        }
+      });
+      }
+    });
+  };
+
+
   return (
     <div className='w-11/12 mx-auto'>
         <NavBar />
         <div className=" grid gap-3 grid-cols-6 min-h-[60vh]">
-          {data.map(items=>{
+          {data&&data.map(items=>{
             return <div key={items._id} className="card  bg-base-100 col-span-6 md:col-span-3 lg:col-span-2 shadow-xl">
             <figure>
               <img className='p-3 border-none rounded-lg'
@@ -25,7 +73,9 @@ const Assignments = () => {
               <p>Difficulty: {items.type}</p>
               <p>Marks: {items.marks}</p>
               <div className="card-actions justify-center">
-                <button className="btn btn-primary">Delete</button>
+                <button className="btn btn-primary" onClick={() => {
+                    handledelete(items._id,items.email);
+                  }}>Delete</button>
                 <button className="btn btn-primary">Update</button>
                 <button className="btn btn-primary">View Assignment</button>
               </div>
